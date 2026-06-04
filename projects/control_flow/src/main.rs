46d1c11906f9d3248957cac7c82e6bfd4b70e688
Alejandro Garcia <garciapinenomanuelalejandro@gmail.com>
2026-06-05 01:48:31 +0200

    control_flow half

--- projects/control_flow/src/main.rs
+++ projects/control_flow/src/main.rs
@@ -1,3 +1,56 @@
 fn main() {
-    println!("Hello, world!");
+    if_let();
+}
+
+fn if_example(){
+    //let number = 3;
+    let number = 7;
+
+    if number < 5{
+        println!("condition was true");
+    }
+    else{
+        println!("condition was false");
+    }
+}
+
+fn if_fails(){
+    let number = 3;
+
+    //Comparing integer instead of bool
+    // if number{
+    //     println!("number was three");
+    // }
+}
+
+fn if_not_zero(){
+    let number = 3;
+
+    if number != 0{
+        println!("number was something other than zero");
+    }
+}
+
+fn multiple_conditions(){
+    let number = 6;
+
+    if number % 4 == 0{
+        println!("number is divisible by 4");
+    }
+    else if number % 3 == 0{
+        println!("number is divisible by 3");
+    }
+    else if number % 2 == 0{
+        println!("number is divisible by 2");
+    }
+    else{
+        println!("number is not divisible by 4, 3 or 2");
+    }
+}
+
+fn if_let(){
+    let condition = false;
+    let number = if condition { 5 } else { 6 };
+
+    println!("The value of number is: {number}");
 }
