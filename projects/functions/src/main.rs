f71cdc68aec368f637ed390dfe0b25f1f0cb0c08
Alejandro Garcia <garciapinenomanuelalejandro@gmail.com>
2026-06-04 23:15:09 +0200

    functions finished

--- projects/functions/src/main.rs
+++ projects/functions/src/main.rs
@@ -1,9 +1,40 @@
 fn main() {
-    println!("Hello, world!");
+    let x = plus_one(five());
 
-    another_function();
+    println!("The value of x is: {x}");
 }
 
 fn another_function(){
     println!("Another function.");
+}
+
+fn another_function_parameter(x: i32){
+    println!("The value of x is {x}");
+}
+
+fn print_labeled_measurement(value: i32, unit_label: char){
+    println!("The measurement is: {value}{unit_label}");
+}
+
+fn proof_assignments_do_not_return_values(){
+    //let x = (let y = 6);
+}
+
+fn expression_inside_scope(){
+    let y = {
+        let x = 3;
+        //x + 1; This is a statement
+        x + 1 //This is an expression
+    };
+
+    println!("The value of y is: {y}");
+}
+
+fn five() -> i32{
+    5
+}
+
+fn plus_one(x: i32) -> i32{
+    x + 1
+    //x + 1; This produces an error
 }
