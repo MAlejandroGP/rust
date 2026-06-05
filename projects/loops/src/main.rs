25e9ddec8ca178911da0bb3d707a11e37c1505b1
Manuel Alejandro García Pineño <garciapinenomanuelalejandro@gmail.com>
2026-06-05 21:54:36 +0200

    loops half

---
+++ projects/loops/src/main.rs
@@ -0,0 +1,46 @@
+fn main() {
+    loop_labels();
+}
+
+fn again_loop(){
+    loop{
+        println!("again!");
+    }
+}
+
+fn counter_loop(){
+    let mut counter = 0;
+
+    let result = loop {
+        counter += 1;
+
+        if counter == 10{
+            break counter * 2;
+        }
+    };
+
+    println!("The result is {result}");
+}
+
+fn loop_labels(){
+    let mut count = 0;
+
+    'counting_up: loop{
+        println!("count = {count}");
+        let mut remaining = 10;
+
+        loop{
+            println!("remaining = {remaining}");
+            if remaining == 9{
+                break;
+            }
+            if count == 2{
+                break 'counting_up;
+            }
+            remaining -= 1;
+        }
+
+        count += 1;
+    }
+    println!("End count = {count}");
+}
